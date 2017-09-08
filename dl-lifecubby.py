05917620708d679ba150d80dbdf44ca911da64d1
Jesse Becker <hawson@gmail.com>
2017-09-08 00:20:17 -0400

    Pylint fixes

--- dl-lifecubby.py
+++ dl-lifecubby.py
@@ -1,12 +1,11 @@
 #!/usr/bin/env python3
 
-import requests
-import json
 import re
 import logging
 import os.path
 import textwrap
-import hashlib
+import json
+import requests
 
 from bs4 import BeautifulSoup
 
@@ -19,12 +18,12 @@ http_client.HTTPConnection.debuglevel = 0
 
 logging.basicConfig()
 logging.getLogger().setLevel(logging.INFO)
-requests_log = logging.getLogger("requests.packages.urllib3")
-requests_log.setLevel(logging.INFO)
-requests_log.propagate = True
+#requests_log = logging.getLogger("requests.packages.urllib3")
+#requests_log.setLevel(logging.INFO)
+#requests_log.propagate = True
 
 
-url = { 
+url = {
     'base':      'https://www.lifecubby.me',
     'login':     'https://www.lifecubby.me/login.html',
     'hub' :      'https://www.lifecubby.me/cubby_hub.html',
@@ -37,13 +36,13 @@ url = {
 credentials_file = './credentials.json'
 
 # for storing filename index values
-index = { }
+index = {}
 
 
-def load_creds(f):
-    j={}
+def load_creds(credfile):
+    j = {}
 
-    j = json.load(open(f))
+    j = json.load(open(credfile))
     j['command'] = 'Login'
     j['_flash'] = 'enabled'
     j['_javascript'] = 'enabled'
@@ -63,25 +62,34 @@ def parse_entry(r):
 
     entry['url'] = r.url
 
-    entry['title'] = re.sub('.*entry title:', '', soup.find(id='entry_title-field').text, flags=re.I).strip()
+    entry['title'] = re.sub(r'.*entry title:',
+                            '',
+                            soup.find(id='entry_title-field').text,
+                            flags=re.I).strip()
 
-    entry['date'] = re.sub('.*date:', '', soup.find(id='date-field').text, flags=re.I).strip()
+    entry['date'] = re.sub(r'.*date:',
+                           '',
+                           soup.find(id='date-field').text,
+                           flags=re.I).strip()
 
     desc = soup.find('div', id=re.compile(r'(activity_purpose__goals__objective-field|observation-field)'))
-    if desc: 
-        entry['description'] = re.sub('^.*(objective|observation):\s+', '', desc.text , flags=re.I|re.M|re.S).strip()
+    if desc:
+        entry['description'] = re.sub(r'^.*(objective|observation):\s+',
+                                      '',
+                                      desc.text,
+                                      flags=re.I|re.M|re.S).strip()
     else:
         entry['description'] = '<none>'
-            
+
 
     #entry_title-field//*[@id="entry_title-field"]
 
     attachments = []
     for attach in soup.find_all(class_='attachment'):
     #for attach in soup.find_all(class_='attachment', id=re.compile(r'^img\d+')):
-        m = re.search('id=(\d+)',attach.a.get('href'))
-        if m:
-            download_url = '{}/{}'.format(url['download'], m.group(1))
+        match = re.search(r'id=(\d+)', attach.a.get('href'))
+        if match:
+            download_url = '{}/{}'.format(url['download'], match.group(1))
             attachments.append(download_url)
         #attachments.append(attach.a.get('href'))
 
@@ -91,40 +99,40 @@ def parse_entry(r):
     return entry
 
 
-def make_filename(title, date,index):
+def make_filename(title, date, index):
 
     # 05/22/2017
-    month,day,year = date.split('/')
-    newdate = '{}-{}-{}'.format(year,month,day)
+    month, day, year = date.split('/')
+    newdate = '{}-{}-{}'.format(year, month, day)
 
-    newtitle = re.sub('\W+','_',title.strip())
-    newtitle = re.sub('[^a-zA-Z0-9]$','',newtitle)
+    newtitle = re.sub(r'\W+', '_', title.strip())
+    newtitle = re.sub(r'[^a-zA-Z0-9]$', '', newtitle)
 
     if index is None:
-        return '{}-{}'.format(newdate,newtitle)
+        return '{}-{}'.format(newdate, newtitle)
 
     if newtitle in index:
-        index[newtitle]+=1
+        index[newtitle] += 1
     else:
-        index[newtitle] =1
+        index[newtitle] = 1
 
-    filename = '{}-{}-{}'.format(newdate,newtitle,index[newtitle])
+    filename = '{}-{}-{}'.format(newdate, newtitle, index[newtitle])
 
-    return filename 
+    return filename
 
 
 
-def fetch_file(url,filename):
+def fetch_file(url, filename):
 
-    r = session_request.get(url)
+    req = session_request.get(url)
 
-    if not r:
-        logging.err('Failed to fetch {}'.format(url))
+    if not req:
+        logging.error('Failed to fetch %s' % str(url))
         return None
 
-    if r.headers['Content-Type'] == 'image/png':
+    if req.headers['Content-Type'] == 'image/png':
         extension = 'png'
-    elif r.headers['Content-Type'] == 'video/quicktime':
+    elif req.headers['Content-Type'] == 'video/quicktime':
         extension = 'mov'
 
     filename = filename + '.' + extension
@@ -133,21 +141,21 @@ def fetch_file(url,filename):
         logging.info("Already have {}  Please remove it to re-fetch.".format(filename))
         return -1
 
-    logging.info("Fetching [{}] to {}.".format(url,filename))
+    logging.info("Fetching [{}] to {}.".format(url, filename))
 
-    if r.status_code == 200:
+    if req.status_code == 200:
         try:
             fd = open(filename, 'wb')
-        except:
-            logging.err("failed opening {} for writing!".format(filename))
+        except OSError:
+            logging.error("failed opening {} for writing!".format(filename))
 
-        fd.write(r.content)
+        fd.write(req.content)
         fd.close()
-        
-    else:
-        logging.err("Something bad happened!")
 
-    return r.status_code
+    else:
+        logging.error("Something bad happened!")
+
+    return req.status_code
 
 
 
@@ -168,15 +176,15 @@ def extract_image(page):
 if __name__ == '__main__':
 
     payload = load_creds(credentials_file)
-    
+
     session_request = requests.session()
     session_request.headers.update({'referer': url['login']})
 
     login = session_request.post(
-            url['login'], 
-            data=payload, 
-            allow_redirects=True, 
-            )
+        url['login'],
+        data=payload,
+        allow_redirects=True,
+        )
 
     print('=================')
     print(login.status_code)
@@ -207,10 +215,10 @@ if __name__ == '__main__':
             logging.info("New login link: {}".format(l))
         else:
             logging.info("Previously seen login link: {}".format(l))
-   
+
 
     # fetch more pages, until it wraps around...
-    # load_more.php returns up to 5 links at a time, 
+    # load_more.php returns up to 5 links at a time,
     # then loops again, in batches of 5
     done = False
     while not done:
@@ -233,7 +241,7 @@ if __name__ == '__main__':
         else:
             bowl.append(soup)
 
-    
+
 
     #print(soup.prettify())
 
@@ -243,7 +251,7 @@ if __name__ == '__main__':
     for soup in bowl:
 
         for link in soup.find_all(class_='preview', limit=4):
-            raw_src=link.get('href')
+            raw_src = link.get('href')
 
             entry_url = url['base'] + raw_src
             #print(entry_url)
@@ -262,8 +270,8 @@ if __name__ == '__main__':
 
                 try:
                     fd = open(metadata_filename, 'w', newline='\n')
-                except:
-                    logging.err("failed opening {} for metadata writing!".format(metadata_filename))
+                except OSError:
+                    logging.error("failed opening {} for metadata writing!".format(metadata_filename))
 
                 fd.write('Title: {}\n'.format(entry['title']))
                 fd.write('Date: {}\n'.format(entry['date']))
@@ -272,10 +280,9 @@ if __name__ == '__main__':
                 fd.close()
 
                 for attachment in entry['attachments']:
-                    filename=make_filename(entry['title'], entry['date'],index)
+                    filename = make_filename(entry['title'], entry['date'], index)
                     #print("new filename={}".format(filename))
                     print("attachment url = " + attachment)
 
                     rc = fetch_file(attachment, filename)
-
 
